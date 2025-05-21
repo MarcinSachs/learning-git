9428627f3b16d2b9a649f35e3e72927a8207856c
Marcin Sachs <marcinsachs@gmail.com>
2025-05-21 18:43:32 +0200

    Add shopping summary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 shopping = {"Piekarnia": ['Chleb', 'Pączek', 'Bułki'],
             "Warzywniak": ['Marchew', 'Seler', 'Rukola']}
+items = 0
 for shop in shopping:
     print(
-        f"Idę do {shop.upper()}, kupuję tu następujące rzeczy: {[item.upper() for item in shopping[shop]]}\n")
+        f"Idę do {shop.upper()}, kupuję tu następujące rzeczy: {[item.upper() for item in shopping[shop]]}")
+    items += len(shopping[shop])
+print(f"W sumie kupuję {items} produktów.")
