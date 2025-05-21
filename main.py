6d946354dad8327f93b6ac13dcdae7cc39693494
Marcin Sachs <marcinsachs@gmail.com>
2025-05-21 18:27:47 +0200

    Create loop for shopping

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 shopping = {"Piekarnia": ['Chleb', 'Pączek', 'Bułki'],
             "Warzywniak": ['Marchew', 'Seler', 'Rukola']}
+for shop in shopping:
+    print(f"Idę do {shop}, kupuję tu następujące rzeczy: {shopping[shop]}\n")
