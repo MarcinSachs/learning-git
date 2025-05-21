c720eea581ebd2eda11888baa476c2e9756d60b3
MarcinSachs <98347077+MarcinSachs@users.noreply.github.com>
2025-05-21 20:36:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ for shop in shopping:
         f"Idę do {shop.upper()}, kupuję tu następujące rzeczy: {[item.upper() for item in shopping[shop]]}")
     items += len(shopping[shop])
 print(f"W sumie kupuję {items} produktów.")
+print("zmodyfikowano")
